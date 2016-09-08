e9ebe5243b1e5f1abf2dbd2f3242c20871bc2002
Yeshe Wingerd <yeshito@gmail.com>
2016-09-07 22:12:00 -0700

    started putting in personal photo, fixed width of landing parallax, need to fix contact glitch

--- script.js
+++ script.js
@@ -1,4 +1,101 @@
+'use strict';
 $(document).ready(function() {
-  $('.parallax').parallax();
-  // Initialize collapse button
+
+  // scroll animation
+  let $animation_elements = $('.flexProject').hide();
+  let $window = $(window);
+
+  $window.trigger('scroll');
+  $window.on('scroll', check_if_in_view);
+  $window.on('scroll resize', check_if_in_view);
+
+  function check_if_in_view() {
+    let window_height = $window.height();
+    let window_top_position = $window.scrollTop();
+    let window_bottom_position = (window_top_position + window_height);
+
+    $.each($animation_elements, function() {
+      let $element = $(this);
+      let element_height = $element.outerHeight();
+      let element_top_position = $element.offset().top;
+      let element_bottom_position = (element_top_position + element_height);
+
+      //check to see if this current container is within viewport
+      if ((element_bottom_position >= window_top_position) && (element_top_position <= window_bottom_position)) {
+        // if (!$element.hasClass('in-view')) {
+        $element.addClass('in-view').show();
+        // }
+      } else {
+        $element.removeClass('in-view').hide();
+      }
+    });
+  }
+  // Navbar scroll
+  function scrollToSection(id) {
+    let section = $(`#${id}`);
+    $('html,body').animate({scrollTop: section.offset().top},'slow');
+  }
+
+  $("a").click(function(e) {
+     scrollToSection(`${e.target.name}`);
+  });
+  // Navbar scroll end
+
+  // Typewriter effect
+  var TxtType = function(el, toRotate, period) {
+        this.toRotate = toRotate;
+        this.el = el;
+        this.loopNum = 0;
+        this.period = parseInt(period, 10) || 2000;
+        this.txt = '';
+        this.tick();
+        this.isDeleting = false;
+    };
+
+    TxtType.prototype.tick = function() {
+        var i = this.loopNum % this.toRotate.length;
+        var fullTxt = this.toRotate[i];
+
+        if (this.isDeleting) {
+        this.txt = fullTxt.substring(0, this.txt.length - 1);
+        } else {
+        this.txt = fullTxt.substring(0, this.txt.length + 1);
+        }
+
+        this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
+
+        var that = this;
+        var delta = 200 - Math.random() * 100;
+
+        if (this.isDeleting) { delta /= 2; }
+
+        if (!this.isDeleting && this.txt === fullTxt) {
+        delta = this.period;
+        this.isDeleting = true;
+        } else if (this.isDeleting && this.txt === '') {
+        this.isDeleting = false;
+        this.loopNum++;
+        delta = 500;
+        }
+
+        setTimeout(function() {
+        that.tick();
+        }, delta);
+    };
+
+    window.onload = function() {
+        var elements = document.getElementsByClassName('typewrite');
+        for (var i=0; i<elements.length; i++) {
+            var toRotate = elements[i].getAttribute('data-type');
+            var period = elements[i].getAttribute('data-period');
+            if (toRotate) {
+              new TxtType(elements[i], JSON.parse(toRotate), period);
+            }
+        }
+        // INJECT CSS
+        var css = document.createElement("style");
+        css.type = "text/css";
+        css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #fff}";
+        document.body.appendChild(css);
+    };
 });
