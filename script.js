3403acbf144efc53304bb886451e7b9169cd2717
Yeshe Wingerd <yeshito@gmail.com>
2016-09-10 18:03:40 -0700

    added favicon and fixed animation

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
 'use strict';
 $(document).ready(function() {
 
-  // scroll animation
-  let $animation_elements = $('.flexProject').hide();
+  // Navbar click scroll effect
+  function scrollToSection(id) {
+    let section = $(`#${id}`);
+    $('html,body').animate({scrollTop: section.offset().top},'slow');
+  }
+
+  $("a").click(function(e) {
+     scrollToSection(`${e.target.name}`);
+  });
+  // End Navbar click scroll effect
+
+  // FadeInUp Scroll Animation
+  let $animation_elements = $('.flexProject');
+  console.log($animation_elements)
   let $window = $(window);
 
   $window.trigger('scroll');
@@ -22,80 +34,12 @@ $(document).ready(function() {
 
       //check to see if this current container is within viewport
       if ((element_bottom_position >= window_top_position) && (element_top_position <= window_bottom_position)) {
-        // if (!$element.hasClass('in-view')) {
-        $element.addClass('in-view').show();
-        // }
+        $element.css('visibility', 'visible').addClass('in-view');
       } else {
-        $element.removeClass('in-view').hide();
+        $element.css('visibility', 'hidden').removeClass('in-view');
       }
     });
   }
-  // Navbar scroll
-  function scrollToSection(id) {
-    let section = $(`#${id}`);
-    $('html,body').animate({scrollTop: section.offset().top},'slow');
-  }
 
-  $("a").click(function(e) {
-     scrollToSection(`${e.target.name}`);
-  });
-  // Navbar scroll end
 
-  // Typewriter effect
-  var TxtType = function(el, toRotate, period) {
-        this.toRotate = toRotate;
-        this.el = el;
-        this.loopNum = 0;
-        this.period = parseInt(period, 10) || 2000;
-        this.txt = '';
-        this.tick();
-        this.isDeleting = false;
-    };
-
-    TxtType.prototype.tick = function() {
-        var i = this.loopNum % this.toRotate.length;
-        var fullTxt = this.toRotate[i];
-
-        if (this.isDeleting) {
-        this.txt = fullTxt.substring(0, this.txt.length - 1);
-        } else {
-        this.txt = fullTxt.substring(0, this.txt.length + 1);
-        }
-
-        this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
-
-        var that = this;
-        var delta = 200 - Math.random() * 100;
-
-        if (this.isDeleting) { delta /= 2; }
-
-        if (!this.isDeleting && this.txt === fullTxt) {
-        delta = this.period;
-        this.isDeleting = true;
-        } else if (this.isDeleting && this.txt === '') {
-        this.isDeleting = false;
-        this.loopNum++;
-        delta = 500;
-        }
-
-        setTimeout(function() {
-        that.tick();
-        }, delta);
-    };
-
-    window.onload = function() {
-        var elements = document.getElementsByClassName('typewrite');
-        for (var i=0; i<elements.length; i++) {
-            var toRotate = elements[i].getAttribute('data-type');
-            var period = elements[i].getAttribute('data-period');
-            if (toRotate) {
-              new TxtType(elements[i], JSON.parse(toRotate), period);
-            }
-        }
-        // INJECT CSS
-        var css = document.createElement("style");
-        css.type = "text/css";
-        css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #fff}";
-        document.body.appendChild(css);
-    };
 });
